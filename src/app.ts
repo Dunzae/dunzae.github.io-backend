61175a0d052830261cd36b333134221fd8c23ce1
dunzae <dunzae99@gmail.com>
2024-10-11 18:53:42 +0900

    @Feat express의 json, static, urlencoded 기능 추가

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDb from "./connectDb"
 
 dotenv.config({path : `.env.${process.env.NODE_ENV}`});
 const app = express();
 const {PORT, MONGO_URI} = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.static("files"));
+app.use(express.urlencoded({extended : true}));
 
 async function main() {
-  const con = await connectDb(MONGO_URI as string);
+  try {
+    const con = await connectDb(MONGO_URI as string);
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`)
+    })
+  } catch(e) {
+    console.log(e);
+  }
 
-  app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
 }
 
 main();
