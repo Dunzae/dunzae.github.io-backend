7b9b94f7b6ca21f864bc24474560d011c21c9a5f
dunzae <dunzae99@gmail.com>
2024-10-11 18:58:00 +0900

    @Feat authRouter를 기본 라우터에 추가

---
+++ src/routes/auth.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const authRouter = express.Router();
+
+authRouter.post("/signIn", 
+    async (req, res) => {
+
+    }
+)
+
+authRouter.post("/signUp",
+    async (req, res) => {
+
+    }
+)
+
+authRouter.post("/check", 
+    async (req, res) => {
+
+    }
+)
+
+export default authRouter;
