187def65848b53369345e7e7843d216d5aa47b9f
dunzae <dunzae99@gmail.com>
2024-10-11 18:57:41 +0900

    @Feat app.ts에서 라우터 파일 분류

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import router from "./routes"
 import connectDb from "./connectDb"
 
 dotenv.config({path : `.env.${process.env.NODE_ENV}`});
@@ -11,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("files"));
 app.use(express.urlencoded({extended : true}));
+
+app.use("/api", router);
 
 async function main() {
   try {
