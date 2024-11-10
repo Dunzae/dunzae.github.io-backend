229e0fc5e57629de32995a2faf0618ef64ac6c8f
dunzae <dunzae99@gmail.com>
2024-11-11 07:33:00 +0900

    @Feat signUp라우터에서 사용하는 에러 상수 정의

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,6 +1,7 @@
 const errors = {
     InputIsEmpty : "The input is empty",
     InputIsInvalid : "The input is invalid",
+    UserExists : "The user exists",
     UserDoesNotExist : "User does not exist",
     PasswordIsNotCorrect : "Password is not correct",
     UnknownError : "Unknown Error"
