b46974219b089f34e66ed8555a214f5f0ca21989
dunzae <dunzae99@gmail.com>
2024-11-11 07:33:29 +0900

    @Feat 회원가입 라우터 생성

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -79,9 +79,75 @@ authRouter.post("/signIn",
     }
 )
 
+/* 회원가입 API
+    @Method POST
+    @body id string
+    @body email string
+    @body password string
+    @status 
+    - 200 
+        {
+            accessToken string
+            refreshToekn string
+        }
+    - 400 The input is empty 아이디나 이메일, 비밀번호가 비어있을 때
+    - 400 The input is invalid 아이디나 이메일, 비밀번호가 정책에 맞지 않는 경우
+    - 401 The user exists 사용자가 이미 존재할 경우(아이디나 이메일이 존재하는 경우)
+    - 500 Unknown Error 알 수 없는 오류가 발생했을 경우
+*/
+
+interface signUpBody {
+    id: string;
+    email: string;
+    password: string;
+}
 authRouter.post("/signUp",
     async (req, res) => {
-        res.send("signUp")
+        const { id, email, password }: signUpBody = req.body;
+        const { InputIsEmpty, InputIsInvalid, UserExists, UnknownError } = errors;
+
+        // 400 아이디나 비밀번호가 비어있는가?
+        if (isInvalidBody(id, email, password)) {
+            res.status(400).json({ error: InputIsEmpty });
+            return;
+        }
+
+        if (id.length < 6 || password.length < 6 || password.search(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~])[^  |\n]+$/gm) == -1
+            || email.search(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) == -1) {
+            res.status(400).json({ error: InputIsInvalid });
+            return;
+        }
+
+        const userModel = model("user", userSchema);
+        try {
+            const user = await userModel.findOne({
+                $or: [{ id }, { email }]
+            });
+
+            if (user !== null) {
+                res.status(400).json({ error: UserExists });
+                return;
+            }
+
+            await new userModel({
+                id, email, password
+            }).save();
+
+            // jwt 생성
+            const accessToken = IssueJwtToken("accessToken", {
+                id,
+                email
+            });
+            const refreshToken = IssueJwtToken("refreshToken");
+
+            res.json({
+                accessToken,
+                refreshToken
+            })
+        } catch (e) {
+            res.status(500).json({ error: UnknownError })
+        }
+
     }
 )
 
