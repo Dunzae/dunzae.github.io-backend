00a741ec80dd03d94bec7a32b03245a3f447cc39
dunzae <dunzae99@gmail.com>
2024-11-12 21:58:19 +0900

    @Feat 토큰 검사시 사용할 수 있는 에러 정의

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,5 +1,7 @@
 const errors = {
     InputIsEmpty : "The input is empty",
+    TokenIsEmpty : "The token is empty",
+    TokenIsInvalid : "The token is invalid",
     InputIsInvalid : "The input is invalid",
     UserExists : "The user exists",
     UserDoesNotExist : "User does not exist",
