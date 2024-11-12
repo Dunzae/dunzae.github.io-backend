e3d32bd940129d96b3bed2dc8bb50c306e37ea34
dunzae <dunzae99@gmail.com>
2024-11-12 22:41:48 +0900

    @Feat verifyJwtToken에 예외처리를 함수내부로 처리

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,11 +3,20 @@ import jwt from "jsonwebtoken"
 export function IssueJwtToken(type: "accessToken" | "refreshToken", payload?: any) {
     const secretKey = process.env.SECRET_KEY as string;
 
-    return jwt.sign(payload ?? {}, secretKey, type === "accessToken" ? { expiresIn: '1h' } : { expiresIn: '1d' });
+    return jwt.sign(
+        { ...payload }, 
+        secretKey, 
+        type === "accessToken" ? { expiresIn: '1s' } : { expiresIn: '1d' }
+    );
 }
 
-export function verifyJwtToken(token : string) {
+export function verifyJwtToken(token: string) {
     const secretKey = process.env.SECRET_KEY as string;
 
-    return jwt.verify(token, secretKey);
+    try {
+        const result = jwt.verify(token, secretKey);
+        return result;
+    } catch(e) {
+        return false;
+    }
 }
