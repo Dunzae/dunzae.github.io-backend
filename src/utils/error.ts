771dec24094c927ae1bdb6f9136f4b2e2d3f911b
dunzae <dunzae99@gmail.com>
2024-11-20 16:51:59 +0900

    @feat 반환하는 에러에 LimitIsEmpty, NoMorePostsAvailable, TypeIsUnknown 에러 추가

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,13 +1,16 @@
 const errors = {
-    InputIsEmpty : "The input is empty",
-    TokenIsEmpty : "The token is empty",
-    TitleIsEmpty : "Title is empty",
-    TokenIsInvalid : "The token is invalid",
-    InputIsInvalid : "The input is invalid",
-    UserExists : "The user exists",
-    UserDoesNotExist : "User does not exist",
-    PasswordIsNotCorrect : "Password is not correct",
-    UnknownError : "Unknown Error"
+    InputIsEmpty: "The input is empty",
+    TokenIsEmpty: "The token is empty",
+    TitleIsEmpty: "Title is empty",
+    LimitIsEmpty: "Limit is empty",
+    TokenIsInvalid: "The token is invalid",
+    InputIsInvalid: "The input is invalid",
+    UserExists: "The user exists",
+    UserDoesNotExist: "User does not exist",
+    PasswordIsNotCorrect: "Password is not correct",
+    NoMorePostsAvailable : "No more posts available",
+    TypeIsUnknown: "Type is unknown",
+    UnknownError: "Unknown Error"
 }
 
 export default errors;
