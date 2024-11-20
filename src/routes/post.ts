ab038c4a47e7b78239e753ec5bca1bfa3f01eeaf
dunzae <dunzae99@gmail.com>
2024-11-21 05:19:11 +0900

    @Feat 단일 포스트 가져오는 api 추가

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -5,8 +5,55 @@ import multer from "multer";
 import errors from "@utils/error";
 import PostSchema from "@models/post";
 import isInvalidBody from "@utils/isInvalidBody";
+import checkMiddleware from "@middlewares/check";
+import { stripHtml } from "string-strip-html";
 
 const postRouter = express.Router();
+
+/* 단일 포스트 가져오기 API
+    @Method get
+    @body id string
+    @status
+    - 200 Ok
+    - 400 There is no id 아이디가 존재하지 않습니다.
+    - 401 There is no post 포스트가 존재하지 않습니다.
+    - 500 Unknown Error 알 수 없는 오류가 발생했을 경우
+*/
+interface GetPostQueryProps {
+    id: string
+}
+
+postRouter.get("/", async (req: Request<{}, {}, {}, GetPostQueryProps>, res) => {
+    const { id } = req.query
+    const { UnknownError } = errors;
+
+    if (isInvalidBody(id)) {
+        res.status(400).json({ error: "There is no id" });
+        return;
+    }
+
+    try {
+        const postModel = model("post", PostSchema);
+        const post = await postModel.findById(id);
+
+        if (post === null) {
+            res.status(400).json({ error: "There is no post" });
+            return;
+        }
+
+        res.json({
+            author : post.author,
+            title : post.title,
+            body : post.body,
+            likeNum : post.like.num,
+            thumbnail : post.thumbnail,
+            createDate : post.createDate
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ error: UnknownError })
+    }
+})
 
 /* 포스트 업로드 API
     @Method POST
@@ -36,7 +83,7 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage });
 
-postRouter.post("/upload", upload.single("thumbnail"), async (req: Request, res: Response) => {
+postRouter.post("/upload", checkMiddleware, upload.single("thumbnail"), async (req: Request, res: Response) => {
     const thumbnail = req.file?.filename;
     const { title, body } = req.body;
     const { UnknownError, TitleIsEmpty } = errors;
@@ -57,13 +104,12 @@ postRouter.post("/upload", upload.single("thumbnail"), async (req: Request, res:
 
         res.sendStatus(200);
     } catch (e) {
-        console.log(e);
         res.status(500).json({ error: UnknownError })
     }
 })
 
 /* 여러 포스트 반환하는 API
-    @Method POST
+    @Method get
     @body type string
     @status 
     - 200 Ok
@@ -72,13 +118,13 @@ postRouter.post("/upload", upload.single("thumbnail"), async (req: Request, res:
     - 500 Unknown Error 알 수 없는 오류가 발생했을 경우
 */
 
-interface uploadBody {
+interface getListQuery {
     type: string;
-    skip: number;
-    limit: number;
+    skip: string;
+    limit: string;
 }
-postRouter.get("/getList", async (req, res) => {
-    const { type, skip, limit } = req.body;
+postRouter.get("/getList", async (req: Request<{}, {}, {}, getListQuery>, res) => {
+    const { type, skip, limit } = req.query;
     const { TypeIsUnknown, LimitIsEmpty, NoMorePostsAvailable, UnknownError } = errors;
 
     if (isInvalidBody(limit)) {
@@ -86,14 +132,14 @@ postRouter.get("/getList", async (req, res) => {
         return;
     }
 
-    if (isInvalidBody(type)) {
-        res.status(400).json({ error: TypeIsUnknown });
-        return;
-    }
+    // if (isInvalidBody(type)) {
+    //     res.status(400).json({ error: TypeIsUnknown });
+    //     return;
+    // }
 
     try {
         const postModel = model("post", PostSchema);
-        const posts = await postModel.find({}).sort([["createDate", 1]]).skip(skip ?? 0).limit(limit)
+        const posts = await postModel.find({}).sort([["createDate", -1]]).skip(parseInt(skip ?? 0)).limit(parseInt(limit))
 
         if (posts === null) {
             res.status(401).json({ error: NoMorePostsAvailable })
@@ -105,7 +151,7 @@ postRouter.get("/getList", async (req, res) => {
                 id: post._id,
                 author: post.author,
                 title: post.title,
-                body: post.body,
+                body: stripHtml(post.body ?? "").result,
                 likeNum: post.like.num,
                 thumbnail: post.thumbnail,
                 createDate: post.createDate,
