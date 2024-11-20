dd1721256968137122f13296a03ca1cbe13f0766
dunzae <dunzae99@gmail.com>
2024-11-21 05:18:32 +0900

    @Feat 이미지 파일 반환하는 미들웨어 추가

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ const options = {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("files"));
+app.use(express.static("public/"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
